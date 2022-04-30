c5726c6a14f02efe777acadc520c0b46697cd2c4
Torin J <torinj24@gmail.com>
2022-04-30 14:27:24 -0600

    Configured Program.cs

--- torin_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Program.cs
+++ torin_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Program.cs
@@ -1,8 +1,29 @@
+using LaunchpadCodeChallenge.API.Services;
+using LaunchpadCodeChallenge.API.Services.Interfaces;
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers(); 
+    builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+}
+
+void ConfigurePipeline(WebApplication app)
+{
+    if (!app.Environment.IsProduction())
+    {
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+    }
+
+    //app.UseAuthorization();
+    app.MapControllers();
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+ConfigureServices(builder);
 
 var app = builder.Build();
 
@@ -10,8 +31,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
+ConfigurePipeline(app);
 
 app.Run();
