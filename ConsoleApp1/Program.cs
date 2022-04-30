2b5dd6c45d2e9024ff599881bf738bb89eb1fed6
Torin J <torinj24@gmail.com>
2022-04-30 15:55:08 -0600

    Add iteration without for loop

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp1;
+
+// Iterate through NameList from start to finish
+QuestionClass.ListRecursion(0);
+
+
---
+++ ConsoleApp1/QuestionClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+         {
+             "Jimmy",
+             "Jeffrey",
+             "John",
+         };
+        
+        // Use recursion to iterate through NamesList
+        public static void ListRecursion(int i)
+        {
+            if (i == NamesList.Count - 1) { 
+                Console.WriteLine(NamesList[i]);
+                return;
+            }
+
+            Console.WriteLine(NamesList[i]);
+            ListRecursion(i + 1);
+        }
+    }
+}
