b15753d05c3ab91c91f5e476ba1008642c7c6438
Torin J <torinj24@gmail.com>
2022-04-30 13:11:47 -0600

    Added employee service

---
+++ torin_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Services/EmployeeService.cs
@@ -0,0 +1,41 @@
+﻿using LaunchpadCodeChallenge.API.Models.Entities;
+using LaunchpadCodeChallenge.API.Services.Interfaces;
+
+namespace LaunchpadCodeChallenge.API.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public IEnumerable<Employee> GetAll()
+        {
+            // Setup test data
+            var departments = new List<Department>();
+            departments.Add(new Department() { Id = new Guid("8acf5ae0-320e-418a-aea8-000000000000"), Address = "11 Accounting Ave", Name = "Accounting", Employees = new List<Employee>() });
+            departments.Add(new Department() { Id = new Guid("8acf5ae0-320e-418a-aea8-111111111111"), Address = "11 Merchants Ave", Name = "Marketing", Employees = new List<Employee>() });
+            var employees = new List<Employee>();
+            employees.Add(new Employee() { Id = new Guid("8acf5ae0-320e-418a-aea8-222222222222"), FirstName = "John", LastName = "Doe", Address = "123 Made Up St", JobTitle = "Accountant", DepartmentName = "Accounting", Department = departments[0] });
+            employees.Add(new Employee() { Id = new Guid("8acf5ae0-320e-418a-aea8-333333333333"), FirstName = "Jane", LastName = "Doe", Address = "127 Made Up St", JobTitle = "Accountant", DepartmentName = "Accounting", Department = departments[0] });
+            employees.Add(new Employee() { Id = new Guid("8acf5ae0-320e-418a-aea8-444444444444"), FirstName = "James", LastName = "Doe", Address = "300 Made Up St", JobTitle = "Marketer", DepartmentName = "Martketing", Department = departments[1] });
+            departments[0].Employees.Add(employees[0]);
+            departments[0].Employees.Add(employees[1]);
+            departments[1].Employees.Add(employees[2]);
+
+            // Return employees
+            return employees;
+        }
+
+        public IList<Employee> ListAll()
+        {
+            // Grab employee data
+            var employees = GetAll().ToList();
+
+            // List all employees in console
+            foreach (var employee in employees)
+            {
+                System.Console.WriteLine(employee.FirstName + " " + employee.LastName);
+            }
+
+            // Return employee list
+            return employees;
+        }
+    }
+}
---
+++ torin_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,10 @@
+﻿using LaunchpadCodeChallenge.API.Models.Entities;
+
+namespace LaunchpadCodeChallenge.API.Services.Interfaces
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAll();
+        IList<Employee> ListAll();
+    }
+}
