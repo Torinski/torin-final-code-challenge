8aaa8c5f6c9d99e2316f10484a041aafd6d4df74
Torin J <torinj24@gmail.com>
2022-04-30 15:32:12 -0600

    Fix service configuration in program.cs

--- torin_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Program.cs
+++ torin_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Program.cs
@@ -1,3 +1,4 @@
+using LaunchpadCodeChallenge.API.Repositories;
 using LaunchpadCodeChallenge.API.Services;
 using LaunchpadCodeChallenge.API.Services.Interfaces;
 
@@ -16,6 +17,11 @@ void ConfigureServices(WebApplicationBuilder builder)
     //        })
     //    );
 
+    // Code to allow program to run without full database implementation:
+    builder.Services.AddDbContext<ApplicationDbContext>();
+
+    // Dependency injection
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
     builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 }
 
@@ -30,6 +36,21 @@ void ConfigurePipeline(WebApplication app)
     //app.UseAuthorization();
     app.MapControllers();
 }
+
+/* To add on database implementation:
+ * 
+// Migration on startup information
+void ExecuteMigrations(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+
+        var context = services.GetService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+}
+*/
 
 var builder = WebApplication.CreateBuilder(args);
 
